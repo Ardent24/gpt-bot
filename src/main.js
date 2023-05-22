3634f78b53004b75076d641eaede51fb88e484b6
Alexander Barsukov <alexander.barsukov@sibers.com>
2023-05-22 23:04:04 +0700

    new

---
+++ src/main.js
@@ -0,0 +1,80 @@
+import { Telegraf, session } from "telegraf";
+import config from "config";
+import { ogg } from "./ogg.js";
+import process from "nodemon";
+import { openai } from "./openai.js";
+import { code } from "telegraf/format";
+
+console.log(config.get("TEST_ENV"));
+
+const bot = new Telegraf(config.get("TELEGRAM_TOKEN"));
+const INITIAL_SESSION = { messages: [] };
+
+bot.use(session());
+
+bot.command("new", async (ctx) => {
+  ctx.session = INITIAL_SESSION;
+  await ctx.reply(
+    "Это новая сессия. Жду вашего голосового или текстового сообщения"
+  );
+});
+
+bot.command("start", async (ctx) => {
+  ctx.session = INITIAL_SESSION;
+  await ctx.reply("Жду вашего голосового или текстового сообщения");
+});
+
+bot.on("text", async (ctx) => {
+  ctx.session ??= INITIAL_SESSION;
+  try {
+    await ctx.reply(code("Сообщение принял. Жду ответ от сервака...."));
+
+    ctx.session.messages.push({
+      role: openai.roles.USER,
+      content: ctx.message.text,
+    });
+
+    const response = await openai.chat(ctx.session.messages);
+
+    ctx.session.messages.push({
+      role: openai.roles.ASSISTANT,
+      content: response.content,
+    });
+
+    await ctx.reply(response.content);
+  } catch (e) {
+    console.log("error", e.message);
+  }
+});
+
+bot.on("voice", async (ctx) => {
+  ctx.session ??= INITIAL_SESSION;
+  try {
+    await ctx.reply(code("Сообщение принял. Жду ответ от сервака...."));
+    const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+    const userId = String(ctx.message.from.id);
+    const oggPath = await ogg.create(link.href, userId);
+    const mp3path = await ogg.toMp3(oggPath, userId);
+
+    const text = await openai.transcription(mp3path);
+    await ctx.reply(code(`Ваш запрос: ${text}`));
+
+    ctx.session.messages.push({ role: openai.roles.USER, content: text });
+
+    const response = await openai.chat(ctx.session.messages);
+
+    ctx.session.messages.push({
+      role: openai.roles.ASSISTANT,
+      content: response.content,
+    });
+
+    await ctx.reply(response.content);
+  } catch (e) {
+    console.log("error", e.message);
+  }
+});
+
+bot.launch();
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
